b76621db0fa97c25238e545903775743953210a9
Eleanor Aranda <earanda60@stuy.edu>
2024-05-29 14:31:34 -0400

    add html

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -5,3 +5,11 @@ import random
 randnum = random.random()
 print('hello there world :D')
 print(randnum)
+
+def make_html_list(data):
+    items = list(map(lambda a: '<li>' + str(a) + '</li>', data))
+    full_items = '\n'.join(items)
+    final = '<ul>' + '\n' + full_items + '\n' + '</ul>'
+    return final
+
+print(make_html_list([90, 99, 97, 89]))
